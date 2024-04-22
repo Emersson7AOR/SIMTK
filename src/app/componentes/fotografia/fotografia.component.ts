f857b8ed03ffbcf809971ed15b4550a56e36f1d7
MilianRiv <j0s3dj3sus@gmail.com>
2024-04-22 15:09:41 -0600

    videos, fotografia

---
+++ src/app/componentes/fotografia/fotografia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fotografia',
+  standalone: true,
+  imports: [],
+  templateUrl: './fotografia.component.html',
+  styleUrl: './fotografia.component.css'
+})
+export class FotografiaComponent {
+
+}
