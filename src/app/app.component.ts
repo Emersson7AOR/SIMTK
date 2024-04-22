87f348af12d90e56dd8986501b3d819be8a02f1b
Emersson <rodrigoalvarezordinola@gmail.com>
2024-04-22 15:14:26 -0600

    Se agrego a home el header, y nosotros, footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componentes/header/header.component";
 import { HomeComponent } from "./componentes/home/home.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, HomeComponent]
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'SIMTK';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,21 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
 
 export const routes: Routes = [
-
+    {
+        path:'',
+        pathMatch:'full',
+        component:HomeComponent,
+        children:[
+            {
+                path:'',
+                pathMatch:'full',
+                component:NosotrosComponent
+            }
+        ]
+    }
 ];
+
+
