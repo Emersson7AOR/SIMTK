a10dd97c7cce979db4febd51b9c561158519bde0
Emersson <rodrigoalvarezordinola@gmail.com>
2024-04-23 01:24:02 -0600

    Se soluciono el problema de las rutas; header y footer estan en app.componente y routle-outlet esta en medio de ellas; archivo <app.routes.ts> estan todas las rutas; se quitaron las rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,23 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { VideosComponent } from './componentes/videos/videos.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FotografiaComponent } from './componentes/fotografia/fotografia.component';
 
 export const routes: Routes = [
     {
         path:'',
         pathMatch:'full',
-        component:HomeComponent,
-        children:[
-            {
-                path:'',
-                pathMatch:'full',
-                component:NosotrosComponent
-            }
-        ]
+        component:NosotrosComponent,
+    },
+    {
+        path:'videos',
+        component:VideosComponent
+    },
+    {
+        path:'fotografias',
+        component:FotografiaComponent
     }
 ];
 
--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  imports: [RouterOutlet, FooterComponent]
 })
 export class HeaderComponent {
+
+  //Inyeccion del Router.
+  router = inject(Router);
+
+  //Funcion que te llevara al componente de videos.
+  irVideos() {
+    this.router.navigate(['videos'])
+  }
+
+  videos = 'videos'
+
+
+
 
 }
